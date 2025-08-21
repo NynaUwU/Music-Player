ad5e206d7931e32372a1456d54d305607c3a395a
Nyna_UwU <posseltigor@gmail.com>
2025-08-21 11:06:06 -0300

    0.0.3

--- app/src/main/java/com/example/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/musicplayer/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.musicplayer;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +14,22 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+   ImageButton sideMenuButton = findViewById(R.id.sideMenuButton);
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        ImageButton sideMenuButton = findViewById(R.id.sideMenuButton);
+
+        sideMenuButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code to execute when the button is clicked
+                // For example, display a Toast message:
+                Toast.makeText(MainActivity.this, "Button clicked!", Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 }
